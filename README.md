a31881f402fe9507cb307eeaeb57a0e7ea421033
skitoo <alexis.couronne@gmail.com>
2013-09-14 07:46:20 +0200

    nettoyage du fichier pelicanconf.py

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 # My personnal blog
 
 ## TODO
-* Nettoyer le fichier pelicanconf
 * Supprimer le fichier publishconf
 * Nettoyer le fichier MakeFile
 * Modifier page 404
--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 AUTHOR = u'Alexis Couronne'
 SITENAME = u'Skitoo'
-SITEURL = 'http://127.0.0.1:8000'
+SITEURL = 'http://skitoo.net'
 
 THEME = 'theme'
 
@@ -26,13 +26,12 @@ CATEGORY_FEED_ATOM = TRANSLATION_FEED_ATOM = None
 AUTHOR_SAVE_AS = CATEGORY_SAVE_AS = ARCHIVES_SAVE_AS = CATEGORIES_SAVE_AS = ARTICLE_LANG_SAVE_AS = PAGE_LANG_SAVE_AS = TAGS_SAVE_AS = False
 PAGE_URL = PAGE_SAVE_AS = '{slug}.html'
 
-STATIC_PATHS = ['images', 'photos', 'downloads']
+STATIC_PATHS = ['images', 'downloads']
 
 RELATIVE_URLS = True
 
 TEMPLATE_PAGES = {
     'journal.html': 'journal.html',
-    #'photos.html': 'photos.html',
     '404.html': '404.html',
     'sitemap.html': 'sitemap.xml',
 }
@@ -41,10 +40,6 @@ LINKS = SOCIAL = None
 
 DEFAULT_PAGINATION = 10
 
-
-def limit(list, number=10):
-    return list[0:number]
-
 JINJA_FILTERS = {
-    'limit': limit,
+    'limit': lambda list, number=10: list[0:number],
 }
