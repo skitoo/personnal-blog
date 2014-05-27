8b798cfe1f4f2ac54d742835e19afc63e4d09008
skitoo <alexis.couronne@gmail.com>
2014-05-27 21:00:16 +0200

    add fabfile

---
+++ fabfile.py
@@ -0,0 +1,66 @@
+
+
+from fabric.api import local, env, hosts
+import fabric.contrib.project as project
+import os
+
+# Local path configuration (can be absolute or relative to fabfile)
+env.deploy_path = 'output'
+DEPLOY_PATH = env.deploy_path
+
+# Remote server configuration
+production = 'skitoo.net'
+dest_path = '/srv/www.skitoo.net'
+
+
+def clean():
+    if os.path.isdir(DEPLOY_PATH):
+        local('rm -rf {deploy_path}'.format(**env))
+        local('mkdir {deploy_path}'.format(**env))
+
+
+def build():
+    local('pelican content -s pelicanconf.py')
+
+
+def rebuild():
+    clean()
+    build()
+
+
+def regenerate():
+    local('pelican content -r -s pelicanconf.py')
+
+
+def serve():
+    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+
+
+def reserve():
+    build()
+    serve()
+
+
+def preview():
+    local('pelican content -s publishconf.py')
+
+
+def cf_upload():
+    rebuild()
+    local('cd {deploy_path} && '
+          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
+          '-U {cloudfiles_username} '
+          '-K {cloudfiles_api_key} '
+          'upload -c {cloudfiles_container} .'.format(**env))
+
+
+@hosts(production)
+def publish():
+    local('pelican content -s publishconf.py')
+    project.rsync_project(
+        remote_dir=dest_path,
+        exclude=".DS_Store",
+        local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        delete=True,
+        extra_opts='--omit-dir-times --no-perms'
+    )
